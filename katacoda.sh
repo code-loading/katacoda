4793f8d11d5399d8394123b3e8f18c9ba26554a7
Ehsan Ahmed <45562767+code-loading@users.noreply.github.com>
2020-11-20 21:38:48 +0100

    added comments

--- katacoda.sh
+++ katacoda.sh
@@ -33,9 +33,11 @@ minikube start
 
 #export kubectl client gitVersion to var named KUBE_VER
 #tr -d \" is used to remove the quotation marks around the version in the KUBE_VER variable
+#using clientVersion instead of serverVersion because at this point the server is still running the older version
 KUBE_VER=$(kubectl version -o json | jq '. | .clientVersion.gitVersion' | tr -d \")
 
 #set kubectl version in minikube config
+#since we still have to start minikube with the --kubernetes-version flag for some reason, this command is kind of useless but I'll leave it in
 minikube config set kubernetes-version $KUBE_VER
 
 #this would make minikube always download whatever version of kubectl is specified, it's better to set it in the minikube config
@@ -54,5 +56,6 @@ echo 'complete -F __start_kubectl k' >>~/.bashrc
 #reinitialise shell for completion to work with aliases
 . ~/.bashrc
 
-#start minikube
+#start minikube again, since reinitialising the shell stops minikube
+#for some reason even with setting the newest version in minikube config, it still starts the server with the older kubectl version, hence passing the --kubernetes-version flag is necessary here
 minikube start --kubernetes-version=$KUBE_VER
