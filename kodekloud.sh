6d36122db8c8b8d2df73f81a66645037eb77aec5
Ehsan Ahmed <45562767+code-loading@users.noreply.github.com>
2020-12-22 00:01:31 +0100

    Create kodekloud.sh

---
+++ kodekloud.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo 'source <(kubectl completion bash)' >>~/.bashrc
+kubectl completion bash >/etc/bash_completion.d/kubectl
+echo 'alias k=kubectl' >>~/.bashrc
+echo 'complete -F __start_kubectl k' >>~/.bashrc
+. ~/.bashrc
+cp ~/.vimrc vimrc
+rm ~/.vimrc
+echo "set sts=2 ts=2 sw=2 et number" > ~/.vimrc && . ~/.vimrc
+alias kcf="kubectl create -f "
+alias kgp="kubectl get pods"
+alias c=clear
